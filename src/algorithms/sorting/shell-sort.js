5d64cfeddfcea803c4d98c5c22cb83974c42253f
jsorz <danislyn@163.com>
2018-04-08 22:30:28 +0800

    update shell sort

--- src/algorithms/sorting/shell-sort.js
+++ src/algorithms/sorting/shell-sort.js
@@ -1,26 +1,12 @@
 import { Compare, defaultCompare } from '../utils/compare';
 
-const sort2 = (arr, compareFn = defaultCompare) => {
-  let len = arr.length;
-  for (let step = Math.floor(len / 2); step > 0; step = Math.floor(step / 2)) {
-    for (let i = step; i < len; i++) {
-      for (let j = i - step; j >= 0 && arr[j] > arr[step + j]; j -= step) {
-        let temp = arr[j];
-        arr[j] = arr[step + j];
-        arr[step + j] = temp;
-      }
-    }
-  }
-  return arr;
-};
-
 const sort = (arr, compareFn = defaultCompare) => {
   let len = arr.length;
   let step = Math.floor(len / 2);
 
   while(step > 0) {
     // 里层循环与插入排序同理
-    // todo step=1 时 n^2
+    // NOTE: step=1 时 O(n^2)，但若已排序，只有 O(n)
     for (let i = step; i < len; i++) {
       let el = arr[i];
       let j = i - step;
